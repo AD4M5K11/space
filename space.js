51c4fb0ea234cc968f28de63a3d84a898638c039
Efderosnay <46681754+Efderosnay@users.noreply.github.com>
2019-03-03 21:28:48 +0000

    Update .js file

--- space.js
+++ space.js
@@ -1,1 +1,32 @@
-nfwf
+function leftArrowPressed() {
+    let canvas = document.getElementById("canvas");
+    let element = document.getElementById("ship");
+    if (canvas.style.width > 1200 + 'px'){
+    element.style.left = parseInt(element.style.left) - 10 + 'px';
+    } 
+    console.log (element.style.left)
+}
+function rightArrowPressed() {
+    let canvas = document.getElementById("canvas");
+    let element = document.getElementById("ship");
+    if (canvas.style.width > 110 +'px'){
+    element.style.left = parseInt(element.style.left) + 10 + 'px';
+    console.log (element.style.left)
+    }
+} 
+
+function move(event){
+    switch (event.keyCode) {
+        case 37:
+        leftArrowPressed();
+        break;
+
+        case 39:
+        rightArrowPressed();
+        break;
+    }
+}
+function gameLoop(){
+    window.addEventListener('keydown', move);
+    
+}
